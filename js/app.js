47c46eb64b17e10b213cac6554c3ba300760f104
“nad1191” <matosnadia@outlook.com>
2022-07-28 01:45:23 -0400

    worked on background and start button

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ let tracker = function(){
 
 }
 
-let canvas;
+
 let context;
 let keys = [];
 
@@ -69,9 +69,9 @@ let mouthData = [
 
 // displaying the mouth on the page
 
-let mouthThree = document.GetElementbyID('mouth');
+// let mouthThree = document.GetElementbyID('mouth');
 
-class Lips {
+class Lips { 
     constructor(x,y,image,width,height){
     this.x = x;
     this.y =y;
@@ -81,7 +81,105 @@ class Lips {
     this.alive = true;
 
     this.render = function(){
-        context.drawImage()
+        ctx.drawImage(this.image, this.x, this.y);
     }
     }
 }
+
+// let woo = new Character(30, 30, mouthThree, 100, 100)
+
+// displaying the start screen, making sure the game reloads every time I refresh the pages, and make a functioning gameloop
+
+let loop;
+let fps = 60;
+let cnv, ctx;
+
+
+window.onload = function(){
+    console.log('gameloop');
+    prepareCanvas();
+    loop = setInterval(() => {
+        update();
+        render();
+    }, 1000/fps);
+}
+//trying to get the background image to appear 
+
+function resize(){
+    let img = document.getElementById('background');
+    let width = img.clientWidth;
+    let height = img.clientHeight; 
+    let cvn = document.getElementById('canvas');
+    canvas.style.width = width;
+    canvas.style.height = height;
+}
+
+function startGame(){
+    myGameArea.start();
+}
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+function update(){
+    myGameArea.clear();
+    myBackground.newPos();
+    myBackground.update();
+    console.log('updating');
+}
+
+function render(){
+    console.log('rendering');
+}
+
+function start() {
+    this.toggleScreen('startScreen' , false);
+    this.toggleScreen('canvas', true);
+    this.prepareCanvas();
+    this.init();
+    this.loop = setInterval(() => {
+        this.update();
+        this.render();
+    }, 1000/this.fps);
+};
+
+function startGame (){
+    // console.log('start game');
+    myGameArea.start();
+}
+
+let myGameArea = {
+    canvas: document.getElementById('canvas'),
+    start : function(){
+        this.canvas.width = 1000;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function(){
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color,x,y){
+    this.width = width;
+    this.height = height;
+    this.x = x; 
+    this.y = y;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x,this.y,this.width, this.height);
+    }
+}
+
+function toggleScreen(id,toggle){
+    let element = document.getElementById(id);
+    let display = (toggle) ? 'block' : 'none';
+    element.style.display = display;
+ 
+};
