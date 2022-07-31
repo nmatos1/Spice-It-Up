3e4adb1dd13301cd64c1f044aa0b62aa268845a9
“nad1191” <matosnadia@outlook.com>
2022-07-30 20:47:27 -0400

    made red box move, working on green box animation

--- js/app.js
+++ js/app.js
@@ -1,185 +1,130 @@
-let mouth = function () {
-
-}
-
-let green = function() {
-
-}
-
-let red = function () {
-
-}
-
-let health = function() {
-
-}
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
+    console.log(context);
+    canvas.width = innerWidth;
+    canvas.height= innerHeight;
+    let width = innerWidth;
+    let height = innerHeight;
+    let mouth;
+    const movement = document.getElementById('movement')
+    let myScore = document.getElementById('score');
 
 
-let score = function(){
+    // inital screen
+    window.addEventListener('DOMContentLoaded', function(){
+        mouth = new Player (75, 75, 'red', 40,80);
+        peppers = new Player(25 , 25,'green', 20, 40);
+    const runGame = setInterval(gameLoop,60);
+    });
 
-}
+    document.addEventListener('keydown', movementHandler);
 
-let timer = function(){
+////canvas rendering
+canvas.setAttribute('height', getComputedStyle(canvas)['height']);
+canvas.setAttribute('width', getComputedStyle(canvas)['width']);
 
-}
-
-let move = function(){
-
-}
-
-let tracker = function(){
-
-}
-
-
-let context;
-let keys = [];
-
-//created a constructor to access player's different functions
-
-let moveTwo = function(data){  
-    if(data === undefined)
-    return;
-    for (let i = 0 ; i < data.length; i++){
-        let setting =data[i];
-        this[setting[0]]=setting[1];
-    }
-    this.alive = true;
-}
-
-//controlling the movement of the mouth
-
-moveTwo.prototype = {
-    update: function (){
-        if(this.alive)
-        {
-            this.move();
-            this.draw();
+/////// Mouth Player///////////////////
+class Player{
+    constructor(x,y,color,width,height){
+        this.position = {
+            x : x,
+            y : y,
         }
+        this.width = width;
+        this.height = height;  
+        this.color = color;
+        this.alive = true; 
+        this.render = function(){
+            context.fillStyle = this.color;
+            context.fillRect(this.position.x, this.position.y,this.width, this.height);
+        }}
     }
-}
 
-// console.log(moveTwo)
 
-// mouth variable specifying the object's methods
+   
 
-let mouthData = [
 
-]
+/////////////////////////////////////////////////
 
-// displaying the mouth on the page
+//creating the movement for the square
 
-// let mouthThree = document.GetElementbyID('mouth');
+function movementHandler(e){
+    console.log('movement', e.key);
 
-class Lips { 
-    constructor(x,y,image,width,height){
-    this.x = x;
-    this.y =y;
-    this.image = image;
-    this.width = width;
-    this.height = height;
-    this.alive = true;
-
-    this.render = function(){
-        ctx.drawImage(this.image, this.x, this.y);
+    switch(e.key){
+        case 'ArrowLeft':
+            mouth.position.x - 10>= 0 ? (mouth.position.x -= 10) : null;
+            break;
+        case 'ArrowRight': 
+        mouth.position.x + 10 <= canvas.width ?(mouth.position.x += 10): null;
+        break;
     }
-    }
-}
-
-// let woo = new Character(30, 30, mouthThree, 100, 100)
-
-// displaying the start screen, making sure the game reloads every time I refresh the pages, and make a functioning gameloop
-
-let loop;
-let fps = 60;
-let cnv, ctx;
-
-
-window.onload = function(){
-    console.log('gameloop');
-    prepareCanvas();
-    loop = setInterval(() => {
-        update();
-        render();
-    }, 1000/fps);
-}
-//trying to get the background image to appear 
-
-function resize(){
-    let img = document.getElementById('background');
-    let width = img.clientWidth;
-    let height = img.clientHeight; 
-    let cvn = document.getElementById('canvas');
-    canvas.style.width = width;
-    canvas.style.height = height;
-}
-
-function startGame(){
-    myGameArea.start();
-}
-
-
-
-
-
-
-/////////////////////////////////////////////////////
-function update(){
-    myGameArea.clear();
-    myBackground.newPos();
-    myBackground.update();
-    console.log('updating');
-}
-
-function render(){
-    console.log('rendering');
-}
-
-function start() {
-    this.toggleScreen('startScreen' , false);
-    this.toggleScreen('canvas', true);
-    this.prepareCanvas();
-    this.init();
-    this.loop = setInterval(() => {
-        this.update();
-        this.render();
-    }, 1000/this.fps);
 };
 
-function startGame (){
-    // console.log('start game');
-    myGameArea.start();
-}
+//game processing
 
-let myGameArea = {
-    canvas: document.getElementById('canvas'),
-    start : function(){
-        this.canvas.width = 1000;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext('2d');
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function(){
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-    }
+function gameLoop(){
+    context.clearRect(0,0, canvas.width, canvas.height);
+    movement.textContent =  `X:${mouth.position.x}\nY:${mouth.position.y}`;
+if(mouth.alive){ 
+    peppers.render();
+    let hit = detectHit(peppers, mouth);
+    
 }
-
-function component(width, height, color,x,y){
-    this.width = width;
-    this.height = height;
-    this.x = x; 
-    this.y = y;
-    this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x,this.y,this.width, this.height);
-    }
-}
-
-function toggleScreen(id,toggle){
-    let element = document.getElementById(id);
-    let display = (toggle) ? 'block' : 'none';
-    element.style.display = display;
- 
+mouth.render();
 };
+
+/////function for multiple peppers
+function newPeppers(){
+peppers.alive = false;
+    setTimeout(function() {
+        let x = Math.floor(Math.random() * canvas.width) - 40;
+        let y = Math.floor(Math.random() * canvas.height) - 80;
+        peppers = new Player(x, y, red, 20, 60);
+    }, 1000);
+    return true;
+}
+//collision 
+
+function detectHit(p1, p2){
+    let hitTest = 
+    p1.y + p1.h > p2 &&
+    p1.y < p2.y + p2.height &&
+    p1.x + p1.width > p2.x &&
+    p1.x <p2.x + p2.width;
+
+    if(hitTest){
+        let newScore = Number(myScore.textContent) + 100;
+        myScore.textContent = newScore;
+        return  
+    }
+
+}
+
+
+///falling objects////
+
+let yPosition = 0;
+
+function myTimer (){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    yPosition = yPosition + 4;
+    context.strokeRect(120, yPosition.newPeppers, 60, 60);
+    context.fillRect(180,yPosition.newPeppers,60,60);
+}
+setInterval(myTimer, 25);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/mouth.js
@@ -0,0 +1,15 @@
+ class player{
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y= 100;
+    }
+    update(){
+
+    }
+    draw(context){
+        context.fillRect(this.x,this.y,this.width, this.height);
+    }
+}
